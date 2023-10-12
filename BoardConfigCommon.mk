59779e8e420c58512b38d301a9b7d873dc71b6ea
bengris32 <bengris32@protonmail.ch>
2023-10-12 18:11:47 +0100

    cupid-common: Set boot image flags
    Change-Id: Ic43af245541417ed809d960059e4e754b0075c99
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,5 +16,15 @@ TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
 TARGET_USES_64_BIT_BINDER := true
 
+# Boot
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x80000 --ramdisk_offset 0x03000000 --dtb_offset 0x4000000 --header_version 0x2
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
+BOARD_KERNEL_CMDLINE := firmware_class.path=/vendor/etc/firmware selinux=1
+BOARD_KERNEL_CMDLINE += androidboot.dtbo_idx=0,1,2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+
 # Inherit the proprietary files
 include vendor/allwinner/cupid-common/BoardConfigVendor.mk
