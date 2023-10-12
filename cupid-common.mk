96fb0d72ce2c7f055dbff12e140c2a274c921093
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-12 18:19:06 +0100

    cupid-common: Enforce RRO for all overlays
    Change-Id: Iceb177c9b5322354b8b9f9ce81a91dc04a2350a8

--- cupid-common.mk
+++ cupid-common.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
