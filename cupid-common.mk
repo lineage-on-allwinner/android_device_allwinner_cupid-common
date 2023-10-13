db3dffe5e7bb65d48695d61464c54b5914934b75
bengris32 <bengris32@protonmail.ch>
2023-10-13 17:49:13 +0100

    cupid-common: Build ClearKey DRM lazy service
    Change-Id: I7fb0d4617d87b6176bb2b4df2196cc1a454f737b
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- cupid-common.mk
+++ cupid-common.mk
@@ -67,6 +67,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4-service-lazy.clearkey
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
