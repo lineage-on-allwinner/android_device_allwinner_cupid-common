9f71ec03058ba76e4916a2ef2fd1ee36ae9fa560
bengris32 <bengris32@protonmail.ch>
2023-10-13 17:56:34 +0100

    cupid-common: Configure and build Gatekeeper stack
    Change-Id: I387eeed5a0f3b947a0161bd49e7e15cd45d22603
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- cupid-common.mk
+++ cupid-common.mk
@@ -78,6 +78,12 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    gatekeeper.cupid
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
