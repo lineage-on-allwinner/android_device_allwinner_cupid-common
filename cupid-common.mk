dfa86adb294df7a83d9111ee1cce4e2590d5c823
bengris32 <bengris32@protonmail.ch>
2023-10-13 18:44:55 +0100

    cupid-common: Configure and build USB stack
    Change-Id: Id8204d33e29cb533a153b65703bce1598952077f
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- cupid-common.mk
+++ cupid-common.mk
@@ -128,6 +128,14 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     tee_supplicant
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # Wireless
 PRODUCT_PACKAGES += \
     wireless_hwinfo
