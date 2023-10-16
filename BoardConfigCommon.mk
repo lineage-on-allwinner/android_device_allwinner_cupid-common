52a83851babb538e5305a56521242554f7be32b3
Alexander Koskovich <akoskovich@pm.me>
2023-10-16 19:31:19 +0100

    cupid-common: Move BT profiles props to vendor
     * Makes it GSI compatible
    
    Change-Id: I10f91824a569dd49d639e7526b32be2bb5ef652c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,7 +67,6 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Properties
-TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Recovery
